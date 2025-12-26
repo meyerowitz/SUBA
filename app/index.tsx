7843f14e96f9c71421d032d5bd06e9b6f04648be
unknown <meyerowitzrebeca@gmail.com>
2025-12-25 20:36:48 -0400

    buehh

--- app/index.tsx
+++ app/index.tsx
@@ -2,7 +2,7 @@ import { Text, View } from "react-native";
 import Navigation from './pages/Pasajero/Navigation';
 import ChooseRol from './ChoseaRol';
 import SplashScreen from "./SplashScreen";
-
+import Login from "./Login";
 export default function Index() {
   return (
       <SplashScreen></SplashScreen>
